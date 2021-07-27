11083f596841daca8ddda381591c0bf4c10c07ed
Cyriacus Ifeanyi <cyriacusifeanyi@gmail.com>
2021-07-27 13:21:56 +0100

    completed 8.17: Listing books

--- src/components/queries.js
+++ src/components/queries.js
@@ -3,21 +3,22 @@ import { gql } from '@apollo/client'
 export const ALL_AUTHORS = gql`
 query {
   allAuthors {
-    id
     name
     born
-    bookCount
   }
 }`
+// bookCount
 
 export const ALL_BOOKS = gql`
 query {
   allBooks2{
+    id
     title
-    author
     published
+    genres
   }
 }`
+// author
 
 export const CREATE_BOOK = gql`
 mutation createBook($title: String!, $author: String!, $published: Int!, $genres: [String!]) {
@@ -29,12 +30,13 @@ mutation createBook($title: String!, $author: String!, $published: Int!, $genres
   ) {
     id
     title
-    author
     published
     genres
   }
 }
 `
+// author
+
 export const EDIT_AUTHOR = gql`
 mutation editAuthor($name: String!, $born: Int!) {
   editAuthor(
